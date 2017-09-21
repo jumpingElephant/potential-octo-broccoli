49e5f901baae7157a71f94c00e07bfb2beb4f1b3
Alexander Rettner <alexander.rettner@googlemail.com>
2017-09-21 19:11:09 +0200

    GUI-Update with spinner

--- install.sh
+++ install.sh
@@ -8,5 +8,4 @@ sudo docker kill consumption-app \
 && mvn clean package -q -Pfat-jar \
 && sudo docker build -t consumption . \
 && sudo docker rm consumption-app \
-&& sudo ./run.sh $1 \
-&& wget --retry-connrefused -q -S -T 0.1 localhost:$1
+&& sudo ./run.sh $1
